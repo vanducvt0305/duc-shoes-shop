8dce83ee269da65dbc3b69415949a3441725aa6b
DucNguyen <vanducvt0305@gmail.com>
2022-09-24 20:28:29 +0700

    f

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,7 @@ import { useFormik } from "formik";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as Yup from "yup";
-import {registerProfileApi} from '../redux/reducers/userReducer'
+import { registerProfileApi } from "../redux/reducers/userReducer";
 
 export default function Register() {
   const { userLogin } = useSelector((state) => state.userReducer);
@@ -21,16 +21,17 @@ export default function Register() {
       email: Yup.string()
         .required("Email không được bỏ trống")
         .email("Email không đúng định dạng !"),
-      password: Yup.string()
-        .required("Password không được bỏ trống"),
-        // .min(1, "pass từ 6 - 32 ký tự !!"),
-      changepassword: Yup.string().required("Confirm Password không được bỏ trống").when("password", {
-        is: (val) => (val && val.length > 0 ? true : false),
-        then: Yup.string().oneOf(
-          [Yup.ref("password")],
-          "Password không khớp, hãy kiểm tra lại !!"
-        ),
-      }),
+      password: Yup.string().required("Password không được bỏ trống"),
+      // .min(1, "pass từ 6 - 32 ký tự !!"),
+      changepassword: Yup.string()
+        .required("Confirm Password không được bỏ trống")
+        .when("password", {
+          is: (val) => (val && val.length > 0 ? true : false),
+          then: Yup.string().oneOf(
+            [Yup.ref("password")],
+            "Password không khớp, hãy kiểm tra lại !!"
+          ),
+        }),
       name: Yup.string().required("Name không được bỏ trống"),
       // .name("Name không đúng định dạng !"),
       phone: Yup.number().required("Phone không được bỏ trống"),
@@ -42,16 +43,16 @@ export default function Register() {
       dispatch(registerProfileApi(values));
     },
   });
-  
+
   const { touched, errors, values, handleBlur, handleChange, handleSubmit } =
     frm;
   return (
-    <div>
+    <div className="container">
       <form
         onSubmit={handleSubmit}
         className="row justify-content-around align-items-start mt-5"
       >
-        <div className="from-group col-5 ">
+        <div className="from-group col-4 ">
           <h3>Email</h3>
           <input
             style={{ height: "54px" }}
@@ -103,61 +104,51 @@ export default function Register() {
           ) : (
             ""
           )}
-          
         </div>
-        <div className="from-group col-5">
-          <h3>Name</h3>
-          <input
-            style={{ height: "54px" }}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.name}
-            className="form-control input-style mb-3"
-            id="name"
-            name="name"
-            type="text"
-            placeholder="Name"
-          />
-          {errors.name && touched.name ? (
-            <span className="text-danger">{errors.name}</span>
-          ) : (
-            ""
-          )}
-          <h3>Phone</h3>
-          <input
-            style={{ height: "54px" }}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.phone}
-            className="form-control input-style mb-3"
-            id="phone"
-            name="phone"
-            type="text"
-            placeholder="Phone"
-          />
-          {errors.phone && touched.phone ? (
-            <span className="text-danger">{errors.phone}</span>
-          ) : (
-            ""
-          )}
-          {/* <h3>Password</h3>
-          <input
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className="form-control input-style"
-            id="password"
-            name="password"
-            type="password"
-            placeholder="Password"
-          />
-          {errors.password && touched.password ? (
-            <span className="text-danger">{errors.password}</span>
-          ) : (
-            ""
-          )} */}
-          <div className="my-4">
-            <div className="d-flex justify-content-between">
-              <div className="d-flex">
+        <div className="from-group col-6">
+          <div className="row flex-direction">
+            <div className="col-8">
+              <div>
+                <h3>Name</h3>
+                <input
+                  style={{ height: "54px" }}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.name}
+                  className="form-control input-style mb-3"
+                  id="name"
+                  name="name"
+                  type="text"
+                  placeholder="Name"
+                />
+                {errors.name && touched.name ? (
+                  <span className="text-danger">{errors.name}</span>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div>
+                <h3>Phone</h3>
+                <input
+                  style={{ height: "54px" }}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.phone}
+                  className="form-control input-style mb-3"
+                  id="phone"
+                  name="phone"
+                  type="text"
+                  placeholder="Phone"
+                />
+                {errors.phone && touched.phone ? (
+                  <span className="text-danger">{errors.phone}</span>
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
+            <div className="my-4 col-8">
+              <div className="d-flex justify-content-between">
                 <div className="gender-text mt-4">Gender</div>
                 <div className="mx-3 align-sefl-center d-flex flex-column">
                   <input
@@ -179,24 +170,19 @@ export default function Register() {
                   />
                   <label htmlFor="gender">Female</label>
                 </div>
+                <div>
+                  <button className="btn btn-update rounded-5" type="submit">
+                    SUBMIT
+                  </button>
+                </div>
               </div>
-              {/* <div>
-                <button className="btn btn-update rounded-5" type="submit">
-                  Update
-                </button>
-              </div> */}
-            </div>
-            <div>
-              {errors.gender && touched.gender ? (
-                <span className="text-danger">{errors.gender}</span>
-              ) : (
-                ""
-              )}
-            </div>
-            <div>
-              <button className="btn btn-update rounded-5" type="submit">
-                SUBMIT
-              </button>
+              <div>
+                {errors.gender && touched.gender ? (
+                  <span className="text-danger">{errors.gender}</span>
+                ) : (
+                  ""
+                )}
+              </div>
             </div>
           </div>
         </div>
